579b5440f2e5185dce0fa400a74b3e87e068829d
KyawHtay <kyaw.htay@gmail.com>
2024-03-30 16:50:30 +0000

    Added CReate/Edit comand and Query

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,35 +1,40 @@
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-    
-        private readonly IMediator _mediator;
-     
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-            
-        }
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities(){
 
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
             
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivities(Guid id){
 
-            return Ok();
+            return await Mediator.Send(new Details.Query{Id=id});
             
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            await Mediator.Send(new Create.Command{Activity=activity});
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult>  Edit(Guid id,Activity activity){
+            activity.Id =  id;
+
+            await Mediator.Send(new Edit.Command{Activity=activity});
+            return Ok();
+
         }
 
     }
